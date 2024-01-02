61103b87614a78611fab2ec51f9c31199353ac9f
thaitina5 <thaitina5@gmail.com>
2024-01-02 11:38:54 +0700

    add missing @Repository annotation

--- CRUD_API/src/main/java/Bai8/SpringDataJpa/dao/StudentRepository.java
+++ CRUD_API/src/main/java/Bai8/SpringDataJpa/dao/StudentRepository.java
@@ -2,6 +2,8 @@ package Bai8.SpringDataJpa.dao;
 
 import Bai8.SpringDataJpa.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 }
